da7d84bc827d47efbfda6e4f2dd6eeef7b046ed4
Andrew <andrewtingnyc@gmail.com>
2026-09-05 11:18:03 -0400

    Emit Q4 tensor-core attention template in diagnostic library

--- benchmarks/q4_mma_tile_probe.cu
+++ benchmarks/q4_mma_tile_probe.cu
@@ -6,6 +6,7 @@
 #define flash_attn_ext_f16 qwen_experimental_q4_mma_kernel
 #define ggml_cuda_flash_attn_ext_mma_f16_case qwen_experimental_q4_mma_case
 #include "ggml-cuda/fattn-mma-f16.cuh"
+template void qwen_experimental_q4_mma_case<256,256,8,8>(ggml_backend_cuda_context &,ggml_tensor *);
 
 extern "C" void qwen_q4_mma_tile_probe(ggml_backend_cuda_context * ctx, ggml_tensor * dst) {
     GGML_ASSERT(dst->src[0]->ne[0] == 256);
